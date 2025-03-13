4f210f4a1b89fea23a7cbd46d78cb87be88b91f6
김재환 <kimjaehwan@gimjaehwan-ui-MacBookPro.local>
2025-03-13 16:23:45 +0900

    feat: 인터페이스 예제 코드 추가

--- learn-ts/src/app.ts
+++ learn-ts/src/app.ts
@@ -1,129 +1,47 @@
-abstract class Department {
-  static fiscalYear = 2025;
-  // private readonly id: string;
-  // private name: string;
-  protected employees: string[] = [];
+// type AddFn = (a: number, b: number) => number;
+interface AddFn {
+  (a: number, b: number): number;
+} // 이런 익명 메소드는 interface로도 구현할 수 있지만 type으로 선언하는게 더 좋다.
 
-  constructor(protected readonly id: string, public name: string) {
-    // this.id = id;
-    // this.name = n;
-    // console.log(this.fiscalYear)
-    // console.log(Department.fiscalYear)
+let add: AddFn;
+
+add = (n1: number, n2: number) => {
+  return n1 + n2;
+};
+
+interface Named {
+  readonly name?: string;
+  outputName?: string; // 선택적 프로퍼티로 만듦
+}
+
+interface Greetable extends Named {
+  greet(phrase: string): void;
+}
+
+// class Person implements Greetable, Named {
+class Person implements Greetable {
+  name?: string;
+  age = 30;
+
+  constructor(n?: string) {
+    if (n) {
+      this.name = n;
+    }
   }
 
-  static createEmployee(name: string) {
-    return { name: name };
-  }
-
-  abstract describe(this: Department): void;
-
-  addEmployee(employee: string) {
-    this.employees.push(employee);
-  }
-
-  printEmployeeInformation() {
-    console.log(this.employees.length);
-    console.log(this.employees);
+  greet(phrase: string) {
+    if (this.name) {
+      console.log(phrase + " " + this.name);
+    } else {
+      console.log('Hi')
+    }
   }
 }
 
-class ITDepartment extends Department {
-  admins: string[];
-  constructor(id: string, admins: string[]) {
-    super(id, "IT");
-    this.admins = admins;
-  }
+let user1: Greetable;
 
-  describe() {
-    console.log("IT Department - ID: " + this.id);
-  }
-}
+user1 = new Person();
+// user1.name = 'Manu';
 
-class AccountingDepartment extends Department {
-  private lastReport: string;
-  private static instance: AccountingDepartment;
-
-  get mostRecentReport() {
-    if (this.lastReport) {
-      return this.lastReport;
-    }
-    throw new Error("No report found.");
-  }
-
-  set mostRecentReport(value: string) {
-    if (!value) {
-      throw new Error("Please pass in a valid value!");
-    }
-    this.addReport(value);
-  }
-
-  private constructor(id: string, private reports: string[]) {
-    super(id, "IT");
-    this.lastReport = reports[0];
-  }
-
-  static getInstance() {
-    if (AccountingDepartment.instance) {
-      return this.instance;
-    }
-    this.instance = new AccountingDepartment("d2", []);
-    return this.instance;
-  }
-
-  describe() {
-    console.log("Accounting Department - ID: " + this.id);
-  }
-
-  addEmployee(name: string) {
-    if (name === "Max") {
-      return;
-    }
-    this.employees.push(name);
-  }
-
-  addReport(text: string) {
-    this.reports.push(text);
-    this.lastReport = text;
-  }
-
-  printReport() {
-    console.log(this.reports);
-  }
-}
-
-const employee1 = Department.createEmployee("Max");
-console.log(employee1, Department.fiscalYear);
-
-const it = new ITDepartment("d1", ["Accounting"]);
-
-it.addEmployee("Max");
-it.addEmployee("Manu");
-
-// accounting.employees[2] = 'anna';
-
-it.describe();
-it.name = "NEW NAME";
-it.printEmployeeInformation();
-
-console.log(it);
-
-// const accounting = new AccountingDepartment("d2", []);
-const accounting = AccountingDepartment.getInstance();
-const accounting1 = AccountingDepartment.getInstance();
-
-console.log(accounting, accounting1);
-
-accounting.mostRecentReport = "Year End Report";
-accounting.addReport("something went wrong...");
-console.log(accounting.mostRecentReport);
-
-accounting.addEmployee("Max");
-accounting.addEmployee("Manu");
-
-// accounting.printReport();
-// accounting.printEmployeeInformation();
-accounting.describe();
-
-// const accountingCopy = {name: 'DUMMY', describe: accounting.describe};
-
-// accountingCopy.describe(); // Department: DUMMY
+user1.greet("Hi there - I am");
+console.log(user1);
